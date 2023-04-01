66d46ad13ddc9ce24dbc3a0425bad7f6835bdd5a
Wallace Gabriel <50117208+Wllc@users.noreply.github.com>
2023-03-31 22:09:34 -0300

    Update Conexao.java

--- Esig_Sample_Project/src/main/java/persistencia/Conexao.java
+++ Esig_Sample_Project/src/main/java/persistencia/Conexao.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Conexao {
 	public static Connection getConexao() throws SQLException {
         String usuario = "postgres";
-        String senha = "postgres";
+        String senha = "****";
         String caminho = "jdbc:postgresql://localhost:5432/TerefasESIG";
         DriverManager.registerDriver(new org.postgresql.Driver());
         return DriverManager.getConnection(caminho, usuario, senha);
